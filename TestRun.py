534f0c941c14f50d7a906db840f56dd58ba7e51b
SuperC0w <bryantiang@hotmail.com>
2024-11-27 09:21:54 -0800

    initial tests with smaller state space

--- TestRun.py
+++ TestRun.py
@@ -4,16 +4,21 @@ import os
 import numpy as np
 from rlglue.types import Action
 import random
+import matplotlib.pyplot as plt
+import tracemalloc
 
+# added to check ram usage
+# tracemalloc.start()
 
 if __name__ == "__main__":
-    nbrReaches = 7
-    habitatSize = 4
+
+    nbrReaches = 3
+    habitatSize = 2
     # initialize fixed start state
     S = np.array([random.randint(1, 3) for i in range(nbrReaches * habitatSize)])
     # initialize environment using fixed start state
-    Exp = InvasiveEnvironment(simulationParameterObj=None, actionParameterObj=None, Bad_Action_Penalty=-10000,fixedStartState=True, nbrReaches=7,
-            habitatSize=4, seed=1)
+    Exp = InvasiveEnvironment(simulationParameterObj=None, actionParameterObj=None, Bad_Action_Penalty=-10000,fixedStartState=True, 
+                              nbrReaches=nbrReaches, habitatSize=habitatSize, seed=1)
     print("Starting fixed state:, ", S)
     Exp.setAgentState(S)
     task_spec = Exp.env_init()
@@ -25,8 +30,14 @@ if __name__ == "__main__":
     # number of steps per episode
     n_steps = 100
 
+    # number of alternate training and evaluating steps
+    n_runs = 1000
+    # used to save the mean values for each run
+    stored_mean = np.ones(n_runs)
+    stored_std_dev = np.ones(n_runs)
+
     # alternate between training and evaluating agent for every 10 episodes
-    for i in range(30):
+    for i in range(n_runs):
         # train agent for 10 episodes
         for episode in range(10):
             Exp.setAgentState(S)
@@ -40,24 +51,9 @@ if __name__ == "__main__":
         
         # freeze learning
         agent.agent_message("freeze learning")
-        
-        # evaluate agent for n episodes
-        # n = 10 
-        # sum = 0
-        # sum_of_squares = 0
-        # for episode in range(n):
-        #     Exp.setAgentState(S)
-        #     # print("initial state", Exp.state)
-        #     first_obs = Exp.env_start()
-        #     first_action = agent.agent_start(first_obs)
-        #     reward_observation_terminal = Exp.env_step(first_action)
-        #     for k in range(n_steps):
-        #         action = agent.agent_step(reward_observation_terminal.r, reward_observation_terminal.o)
-        #         reward_observation_terminal = Exp.env_step(action)
-        #         sum += reward_observation_terminal.r
-        #         sum_of_squares += sum**2 
-        
-
+        # freeze exploring
+        agent.agent_message("freeze exploring")
+                
         # evaluate agent for n episodes (updated)
         n = 10 
         for episode in range(n):
@@ -76,12 +72,14 @@ if __name__ == "__main__":
             sum_of_squares += sum**2  # outside the for loop for global variance approach 
 
         mean = sum / n
+        stored_mean[i] = mean
         if n <= 1:
             print("Not enough episodes to calculate variance.")
             variance = 0
         else:
             variance = (sum_of_squares - n * mean * mean) / (n - 1.0)
         standard_dev = np.sqrt(variance)
+        stored_std_dev[i] = standard_dev
         
 
         print("run: ", i)
@@ -97,3 +95,21 @@ if __name__ == "__main__":
         
         # unfreeze learning
         agent.agent_message("unfreeze learning")
+        # unfreeze exploring
+        agent.agent_message("unfreeze exploring")
+        
+    # added to check ram usage
+    # # Display the current memory usage
+    # snapshot = tracemalloc.take_snapshot()
+    # top_stats = snapshot.statistics("lineno")
+
+    # print("[ Top 10 Memory Consumers ]")
+    # for stat in top_stats[:10]:
+    #     print(stat)
+    
+    plt.plot(stored_mean)
+    plt.title("mean")
+    plt.figure()
+    plt.plot(stored_std_dev)
+
+    plt.show()
